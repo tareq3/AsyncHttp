fbd4d03301ecb22e88c18a0f13912cdf020d0cc8
tareq3 <mti.tareq3@gmai.com>
2019-02-14 21:41:12 +0600

    Pull refresh or swipe refresh  added

--- app/src/main/java/com/example/rakib/asynchttp/MainActivity.java
+++ app/src/main/java/com/example/rakib/asynchttp/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.rakib.asynchttp;
 
 import android.os.Bundle;
+import android.support.v4.widget.SwipeRefreshLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
@@ -17,53 +18,69 @@ import com.example.rakib.asynchttp.api.ApiServices;
 import com.example.rakib.asynchttp.model.Category;
 import com.example.rakib.asynchttp.model.CategoryResponse;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import retrofit2.Call;
 import retrofit2.Callback;
 import retrofit2.Response;
 
-public class MainActivity extends AppCompatActivity  {
+public class MainActivity extends AppCompatActivity implements SwipeRefreshLayout.OnRefreshListener, ListItemAdapter.ItemClickListener {
 
     //Todo: Store the Api key right here
-    private final static String API_KEY="7546c1b03b96db7a7aad6f3e05e9ee81";
+    private final static String API_KEY = "7546c1b03b96db7a7aad6f3e05e9ee81";
 
-RecyclerView mRecyclerView;
-RecyclerView.LayoutManager mLayoutManager;
+    RecyclerView mRecyclerView;
+    RecyclerView.LayoutManager mLayoutManager;
 
-ListItemAdapter listItemAdapter;
+    ListItemAdapter listItemAdapter;
 
-List<Category> categoryResponseList;
+    List<Category> categoryResponseList = new ArrayList<>();
+
+    private SwipeRefreshLayout mSwipeRefreshLayout;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        mRecyclerView=findViewById(R.id.list_Recycler);
+        mSwipeRefreshLayout = findViewById(R.id.swiperefresh);
+        mSwipeRefreshLayout.setOnRefreshListener(this);
+
+        mRecyclerView = findViewById(R.id.list_Recycler);
         mRecyclerView.setHasFixedSize(true);
-        mLayoutManager=new LinearLayoutManager(this);
+        mLayoutManager = new LinearLayoutManager(this);
         mRecyclerView.setLayoutManager(mLayoutManager);
+
+        listItemAdapter = new ListItemAdapter(MainActivity.this, categoryResponseList, this); //if we don't need to use listener can use null instead of this
+
+        mRecyclerView.setAdapter(listItemAdapter);
 
         loadData();
 
 
     }
 
+    /*for loading data using retrofit*/
     private void loadData() {
-        ApiServices apiServices= ApiClient.getClient().create(ApiServices.class);
+        ApiServices apiServices = ApiClient.getClient().create(ApiServices.class);
 
-        Call<CategoryResponse> call=apiServices.getCategory();
+        Call<CategoryResponse> call = apiServices.getCategory();
 
         call.enqueue(new Callback<CategoryResponse>() {
 
 
             @Override
             public void onResponse(Call<CategoryResponse> call, Response<CategoryResponse> response) {
-                categoryResponseList=response.body().getCategory();
-                Log.d("Tareq","Number of Categories"+categoryResponseList.size());
+                categoryResponseList = response.body().getCategory();
+                Log.d("Tareq", "Number of Categories" + categoryResponseList.size());
 
-                listItemAdapter=new ListItemAdapter(MainActivity.this,categoryResponseList);
-                mRecyclerView.setAdapter(listItemAdapter);
+                listItemAdapter.updateAdapter((ArrayList<Category>) categoryResponseList);
+
+
+                listItemAdapter.notifyDataSetChanged();
+                // stopping swipe refresh
+                mSwipeRefreshLayout.setRefreshing(false);
             }
 
             @Override
@@ -73,19 +90,28 @@ List<Category> categoryResponseList;
         });
     }
 
-    @Override
-    protected void onResume() {
-        super.onResume();
 
-    }
-
+    /*On long press context*/
     @Override
     public boolean onContextItemSelected(MenuItem item) {
-        if(item.getTitle().equals("New"))
-        {
+        if (item.getTitle().equals("New")) {
             Toast.makeText(this, "New Activity is not Added yet from:  "
                     + item.getOrder(), Toast.LENGTH_SHORT).show();
         }
         return super.onContextItemSelected(item);
+    }
+
+    /*on pull refresh update the adapter*/
+    @Override
+    public void onRefresh() {
+
+        Toast.makeText(this, "Refresh", Toast.LENGTH_SHORT).show();
+        loadData();
+    }
+
+    /*On Item click*/
+    @Override
+    public void onClick(View view, int position, boolean isLongClick) {
+        Toast.makeText(this, "pos: " + position, Toast.LENGTH_SHORT).show();
     }
 }
--- app/src/main/java/com/example/rakib/asynchttp/adapter/ListItemAdapter.java
+++ app/src/main/java/com/example/rakib/asynchttp/adapter/ListItemAdapter.java
@@ -1,5 +1,6 @@
 package com.example.rakib.asynchttp.adapter;
 
+import android.app.Activity;
 import android.support.annotation.NonNull;
 import android.support.v7.widget.RecyclerView;
 import android.view.ContextMenu;
@@ -9,36 +10,41 @@ import android.view.ViewGroup;
 import android.widget.TextView;
 import android.widget.Toast;
 
-import com.example.rakib.asynchttp.MainActivity;
 import com.example.rakib.asynchttp.R;
 import com.example.rakib.asynchttp.model.Category;
 import com.example.rakib.asynchttp.model.CategoryResponse;
 
+import java.util.ArrayList;
 import java.util.List;
-import java.util.zip.Inflater;
 
-class ListItemViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener,View.OnCreateContextMenuListener{
+public class ListItemAdapter extends RecyclerView.Adapter<ListItemAdapter.ListItemViewHolder>{
 
-    T_ItemClickListener t_itemClickListener;
-    TextView name,no_item;
+    private ItemClickListener mItemClickListener; /*Interface for item click callback*/
 
-    public ListItemViewHolder(View itemView) {
+    /*View Holder Class*/
+    class ListItemViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener,View.OnCreateContextMenuListener{
+
+
+    TextView name,no_item; /*Views */
+
+    /* Constructor*/
+    ListItemViewHolder(View itemView) {
         super(itemView);
         name=itemView.findViewById(R.id.item_name);
         no_item=itemView.findViewById(R.id.item_no);
-        itemView.setOnClickListener(this);
-        itemView.setOnCreateContextMenuListener(this);
+
+        itemView.setOnClickListener(this); /*for single click listener*/
+        itemView.setOnCreateContextMenuListener(this);/*for long press listener*/
     }
 
-    public void set_ItemClickListener(T_ItemClickListener itemClickListener){
-        this.t_itemClickListener=itemClickListener;
-    }
-
+    /*passing item click callback from view to interface ItemClickListener */
     @Override
     public void onClick(View v) {
-        t_itemClickListener.onClick(v,getAdapterPosition(),false);
+        /*if listener available */
+        if(mItemClickListener!=null)    mItemClickListener.onClick(v,getAdapterPosition(),false);
     }
 
+    /* for long press */
     @Override
     public void onCreateContextMenu(ContextMenu menu, View v, ContextMenu.ContextMenuInfo menuInfo) {
 
@@ -46,40 +52,61 @@ class ListItemViewHolder extends RecyclerView.ViewHolder implements View.OnClick
         menu.add(0,0,getAdapterPosition(),"New");
     }
 }
-public class ListItemAdapter extends RecyclerView.Adapter<ListItemViewHolder>{
-    MainActivity mainActivity;
+/*End of ViewHolder Class*/
+
+   /*Member variables of adapter class */
+    Activity mActivity; /* */
     List<Category> categoryList;
 
-    public ListItemAdapter(MainActivity mainActivity, List<Category> categoryList) {
-        this.mainActivity = mainActivity;
+    /*Constructor with click listener*/
+    public ListItemAdapter(Activity mActivity, List<Category> categoryList, ItemClickListener itemClickListener) {
+        this.mActivity = mActivity;
         this.categoryList = categoryList;
+        this.mItemClickListener=itemClickListener;
+    }
+
+
+    /* for updating adapter on refresh*/
+    public void updateAdapter(ArrayList<Category> list){
+        categoryList=  list;
     }
 
     @NonNull
     @Override
     public ListItemViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        LayoutInflater inflater= LayoutInflater.from(mainActivity.getBaseContext());
+        LayoutInflater inflater= LayoutInflater.from(mActivity.getBaseContext());
         View view=inflater.inflate(R.layout.recycler_item,parent,false);
 
         return new ListItemViewHolder(view);
     }
+
 
     @Override
     public void onBindViewHolder(@NonNull ListItemViewHolder holder, int position) {
 
         holder.name.setText(categoryList.get(position).getCName());
         holder.no_item.setText(categoryList.get(position).getNoItem().toString());
-        holder.set_ItemClickListener(new T_ItemClickListener() {
+
+        //Another way not recommended to handle item click
+      /*  holder.seItemClickListener(new ItemClickListener() {
             @Override
             public void onClick(View view, int position, boolean isLongClick) {
 
-                Toast.makeText(mainActivity.getBaseContext(), "I am no:"+position, Toast.LENGTH_SHORT).show();
+                Toast.makeText(mActivity.getBaseContext(), "I am no:"+position, Toast.LENGTH_SHORT).show();
             }
-        });
+        });*/
     }
 
     @Override
     public int getItemCount() {
         return categoryList.size();
     }
+
+
+    /* Interface for Item click callback */
+    public interface ItemClickListener {
+
+        void onClick(View view, int position, boolean isLongClick);
+    }
+
 }
